5cc72ef608c8b347320163bd89eeaaf2e653830c
Rafly Andrian <raflyandrian@gmail.com>
2024-02-01 21:43:00 +0700

    creating factory (Furniture is done)

---
+++ database/factories/AdminFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class AdminFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'uuid' => fake()->uuid(),
+            'username' => fake()->username(),
+            'password' => Hash::make('password'),
+            'name' => fake()->name(),
+            'role' => fake()->randomElement(['1','2']),
+        ];
+    }
+}
---
+++ database/factories/FurnitureFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class FurnitureFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'uuid' => $this->faker->uuid(),
+            'image' => $this->faker->word().'.jpg',
+            'code' => $this->faker->bothify('?#?##?'),
+            'description' => $this->faker->words(3, true),
+            'category' => $this->faker->randomElement(['Indoor','Outdoor','Handicraft','Root']),
+            'wood_type' => $this->faker->randomElement(['Teak wood','Tiger wood','Mahogany wood','Root']),
+            'width' => $this->faker->randomNumber(3,true),
+            'depth' => $this->faker->randomNumber(3,true),
+            'height' => $this->faker->randomNumber(3,true),
+            'price' => $this->faker->randomNumber(3,true),
+            //
+        ];
+    }
+}
