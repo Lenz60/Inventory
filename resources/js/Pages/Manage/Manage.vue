2f9fea3d1a56b99d025de63c4dfd8fb09301e379
Rafly Andrian <raflyandrian@gmail.com>
2024-03-04 21:57:59 +0700

    Basic layout for manage admin

---
+++ resources/js/Pages/Manage/Manage.vue
@@ -0,0 +1,34 @@
+<template>
+    <Head title="Manage" />
+
+    <AuthenticatedLayout>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-neutral overflow-hidden shadow-sm card">
+                    <div class="p-6 text-neutral-content">
+                        You're logged in!
+                    </div>
+                    <div class="p-6 text-neutral-content">
+                        Hello Master Admin
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+export default {
+    components: {
+        AuthenticatedLayout,
+        Head,
+    },
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
