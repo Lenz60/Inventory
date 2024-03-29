7dbe3b000ec50c8390642b072dfff6462b52944a
Rafly Andrian <raflyandrian@gmail.com>
2024-03-29 22:52:38 +0700

    Edit text color tobe can be readable

--- resources/js/Components/InputError.vue
+++ resources/js/Components/InputError.vue
@@ -8,7 +8,7 @@ defineProps({
 
 <template>
     <div v-show="message">
-        <p class="text-sm text-error">
+        <p class="text-sm text-red-600">
             {{ message }}
         </p>
     </div>
