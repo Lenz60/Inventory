25e7a363959f727bd611497dc20c918706791a04
Rafly Andrian <raflyandrian@gmail.com>
2024-06-27 17:31:58 +0700

    feat: Add OrderItems model and migration

---
+++ database/migrations/2024_06_20_143116_create_order_items.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('order_id');
+            $table->string('user_id');
+            $table->string('furniture_id');
+            $table->string('preorder');
+            $table->float('price');
+            $table->string('qty');
+            $table->float('total_price');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('user_id')->references('uuid')->on('users')->onDelete('cascade');
+            $table->foreign('furniture_id')->references('uuid')->on('furniture')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
