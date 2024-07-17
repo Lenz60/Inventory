c8ba3df102fdb37932e0813c01f91e237207edb8
Rafly Andrian <raflyandrian@gmail.com>
2024-07-17 10:01:54 +0700

    Added new database to integrate device manager

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class DeviceController extends Controller
@@ -10,6 +11,28 @@ class DeviceController extends Controller
     //
 
     public function index(){
-        return Inertia::render('Device');
+        $conn = DB::connection('mysql_baileys');
+        $creds = $conn->table('session')->select('data')->where('id', 'creds')->first();
+        if($creds === null){
+            $deviceName = null;
+            $deviceId = null;
+            // dd($deviceName, $deviceId);
+
+            return Inertia::render('Device', [
+                'deviceName' => $deviceName,
+                'deviceId' => $deviceId,
+            ]);
+
+        }else{
+            $jsonCreds = json_decode($creds->data, true);
+            $deviceName = $jsonCreds['me']['name'];
+            $deviceId = $jsonCreds['me']['id'];
+            // dd($deviceName, $deviceId);
+
+            return Inertia::render('Device', [
+                'deviceName' => $deviceName,
+                'deviceId' => $deviceId,
+            ]);
+        }
     }
 }
